b27263734b213bcfd223ea40c8bb8ea9aeec8ae0
Michael Hampo <mjhampo@gmail.com>
2022-05-17 01:28:25 +0100

    pushing to github

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ class Ratings(db.Model):
         return '<whiskey %r>' % self.whiskey
 
 class Collection(db.Model):
-    bottle = db.Column(db.String(200), primary_key=True)
+    bottle_id = db.Column(db.Integer, primary_key=True)
+    bottle = db.Column(db.String(200))
     whiskey_type = db.Column(db.String(200), default=0)
     proof = db.Column(db.Float, default=0)
     avg_rating = db.Column(db.Float, default=0)
@@ -27,22 +28,30 @@ class Collection(db.Model):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    #ratings = Ratings.query.order_by(Ratings.date_drank).all()
+    collection = Collection.query.order_by(Collection.whiskey_type, Collection.bottle).all()
+    return render_template('index.html', collection=collection)
+
+@app.route('/collection', methods=['POST', 'GET'])
+def collection():
     print(request.method)
     if request.method == 'POST':
+        bottle_id = len(Collection.query.all())
+        print(bottle_id)
         bottle = request.form['bottle']
         w_type = request.form['type']
         proof = request.form['proof']
-        new_bottle = Collection(bottle=bottle, whiskey_type=w_type, proof=proof)
+        new_bottle = Collection(bottle_id=bottle_id, bottle=bottle, whiskey_type=w_type, proof=proof)
         try:
             db.session.add(new_bottle)
             db.session.commit()
-            return redirect('/')
+            return redirect('/collection')
         except:
             return 'There was an issue adding to your collection'
     else:
         ratings = Ratings.query.order_by(Ratings.date_drank).all()
-        collection = Collection.query.order_by(Collection.date_added).all()
-        return render_template('index.html', ratings=ratings, collection=collection)
+        collection = Collection.query.order_by(Collection.whiskey_type, Collection.bottle).all()
+        return render_template('collection.html', ratings=ratings, collection=collection)
 
 
 @app.route('/data')
@@ -58,13 +67,10 @@ def proof_round(p):
 
 @app.route('/rate/<string:id>', methods=['GET', 'POST'])
 def rate(id):
-    print("here")
     bottle = Collection.query.get_or_404(id)
 
     if request.method == 'POST':
         task.content = request.form['content']
-
-        print("here1")
         try:
             db.session.commit()
             return redirect('/')
@@ -72,20 +78,36 @@ def rate(id):
             return 'There was an issue updating your task'
 
     else:
-        print("here2")
         return render_template('rate.html', bottle=bottle)
 
-""" @app.route('/delete/<int:id>')
+@app.route('/delete/<string:id>')
 def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
+    bottle = Collection.query.get_or_404(id)
 
     try:
-        db.session.delete(task_to_delete)
+        db.session.delete(bottle)
         db.session.commit()
         return redirect('/')
     except:
-        return 'There was a problem deleting that task'
- """
+        return 'There was a problem deleting the bottle'
+
+@app.route('/edit/<string:id>', methods=['GET', 'POST'])
+def edit(id):
+    bottle = Collection.query.get_or_404(id)
+
+    if request.method == 'POST':
+        bottle.bottle = request.form['bottle']
+        bottle.whiskey_type = request.form['type']
+        bottle.proof = request.form['proof']
+        try:
+            db.session.delete(bottle)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was a problem deleting the bottle'
+    else:
+        return render_template("edit.html", bottle=bottle)
+ 
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
