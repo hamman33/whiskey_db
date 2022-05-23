02f2600d0a1ecd4c91688bf2e98b1ae4af44c682
Michael Hampo <mjhampo@gmail.com>
2022-05-23 18:34:31 -0400

    Finished initial features

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ db = SQLAlchemy(app)
 class Ratings(db.Model):
     rating_num = db.Column(db.Integer, primary_key=True)
     bottle_id = db.Column(db.Integer, nullable=False)
+    bottle = db.Column(db.String(200))
     rating = db.Column(db.Integer, nullable=False)
-    drinker = db.Column(db.String(200))
+    drinker = db.Column(db.String(50))
     date_drank = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -19,7 +20,8 @@ class Ratings(db.Model):
 class Collection(db.Model):
     bottle_id = db.Column(db.Integer, primary_key=True)
     bottle = db.Column(db.String(200))
-    whiskey_type = db.Column(db.String(200), default=0)
+    owner = db.Column(db.String(50))
+    whiskey_type = db.Column(db.String(50), default=0)
     proof = db.Column(db.Float, default=0)
     avg_rating = db.Column(db.Float, default=0)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
@@ -43,7 +45,8 @@ def collection():
         bottle = request.form['bottle']
         w_type = request.form['type']
         proof = request.form['proof']
-        new_bottle = Collection(bottle_id=bottle_id, bottle=bottle, whiskey_type=w_type, proof=proof)
+        owner = request.form['owner']
+        new_bottle = Collection(bottle_id=bottle_id, bottle=bottle, whiskey_type=w_type, proof=proof, owner=owner)
         try:
             db.session.add(new_bottle)
             db.session.commit()
@@ -54,6 +57,17 @@ def collection():
         ratings = Ratings.query.order_by(Ratings.date_drank).all()
         collection = Collection.query.order_by(Collection.whiskey_type, Collection.bottle).all()
         return render_template('collection.html', ratings=ratings, collection=collection)
+
+@app.route('/ratings', methods=['POST', 'GET'])
+def ratings():
+    print(request.method)
+    if request.method == 'POST':
+        #do nothing
+        pass
+    else:
+        ratings = Ratings.query.order_by(Ratings.date_drank).all()
+        collection = Collection.query.order_by(Collection.whiskey_type, Collection.bottle).all()
+        return render_template('ratings.html', ratings=ratings, collection=collection)
 
 
 @app.route('/data')
@@ -75,7 +89,7 @@ def rate(id):
         if(not rating.isdigit()):
             return 'Please enter a number from 1-10'
         
-        new_rating = Ratings(rating_num=len(Ratings.query.all()), bottle_id=id, rating=int(rating))
+        new_rating = Ratings(rating_num=len(Ratings.query.all()), bottle_id=id, bottle= bottle_obj.bottle, rating=int(rating))
         try:
             db.session.add(new_rating)
             db.session.commit()
@@ -104,7 +118,7 @@ def delete(id):
         for rat in ratings:
             db.session.delete(rat)
         db.session.commit()
-        return redirect('/')
+        return redirect('/collection')
     except:
         return 'There was a problem deleting the bottle'
 
@@ -119,13 +133,16 @@ def edit(id):
         try:
             db.session.delete(bottle)
             db.session.commit()
-            return redirect('/')
+            return redirect('/collection')
         except:
             return 'There was a problem deleting the bottle'
     else:
         return render_template("edit.html", bottle=bottle)
  
 
-if __name__ == "__main__":
+def main():
     app.run(debug=True, host="0.0.0.0")
     db.create_all()
+
+if __name__ == "__main__":
+    main()
